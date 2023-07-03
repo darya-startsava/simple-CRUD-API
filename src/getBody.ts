044e1bdfb0677d3ffc4967a6be59b8ff1148eed7
darya-startsava <daryastartsava@gmail.com>
2023-07-03 20:01:28 +0200

    feat: catch error if body is not in JSON format

--- src/getBody.ts
+++ src/getBody.ts
@@ -13,7 +13,14 @@ const getBody = (
   request.on('end', () => {
     request.data = Buffer.concat(data).toString();
     if (request.headers['content-type'] === 'application/json') {
-      request.body = JSON.parse(request.data);
+      try {
+        request.body = JSON.parse(request.data);
+      } catch {
+        response.statusCode = 400;
+        response.write('Wrong body, it should be in JSON format');
+        response.end();
+        return;
+      }
     }
 
     next(request, response);
